265a58668f986ceb565f613cae84279328aadd07
tanvi-12 <69550520+tanvi-12@users.noreply.github.com>
2021-02-16 21:37:00 +1100

    model and log page

--- static/js/datalog.js
+++ static/js/datalog.js
@@ -21,3 +21,15 @@ function init(){
 
 init();
 
+// Select the button
+function myFunction() {
+    const filter = document.querySelector('#myInput').value.toUpperCase();
+    const trs = document.querySelectorAll('#table-log tr:not(.header)');
+    trs.forEach(tr => tr.style.display = [...tr.children].find(td => td.innerHTML.toUpperCase().includes(filter)) ? '' : 'none');
+  }
+ 
+myFunction();
+
+
+
+
